21b5cf5d3e25ede71d39212593c59754256016a7
Boris Guéry <guery.b@gmail.com>
2016-04-04 11:30:41 +0200

    initial commit - bundle skeleton

---
+++ Bgy/OAuth2ServerBundle/BgyOAuth2ServerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bgy\OAuth2ServerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BgyOAuth2ServerBundle extends Bundle
+{
+}
---
+++ Bgy/OAuth2ServerBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Bgy\OAuth2ServerBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('bgy_o_auth2_server');
+
+        $rootNode
+            ->children()
+                ->arrayNode('authorization_server')
+                    ->children()
+                        ->arrayNode('storage')
+                            ->children()
+                                ->scalarNode('access_token')->end()
+                                ->scalarNode('refresh_token')->end()
+                                ->scalarNode('client_token')->end()
+                            ->end()
+                        ->end()
+                        ->scalarNode('token_generator')->end()
+                        ->booleanNode('always_generate_a_refresh_token')
+                            ->defaultTrue()
+                        ->end()
+                        ->booleanNode('always_require_a_client')
+                            ->defaultTrue()
+                        ->end()
+                        ->numericNode('access_token_ttl')
+                            ->defaultValue(3600)
+                        ->end()
+                        ->arrayNode('grant_types')
+                            ->prototype('scalar')->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+
+        return $treeBuilder;
+    }
+}
